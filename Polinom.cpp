0353effefd8744cbc47557591c975619467dcaea
Taisiia <tayafenz@gmail.com>
2020-03-08 23:15:20 +0200

    fixed bags

--- Polinom.cpp
+++ Polinom.cpp
@@ -22,30 +22,30 @@ Polinom::Polinom(int _power, std::vector<int> keys) {
     }
 }
 
-Polinom::PElement * Polinom::makeItem(int value) {
-    auto * el = new PElement;
+Polinom::PElement *Polinom::makeItem(int value) {
+    auto *el = new PElement;
     el->key = value;
     el->next = nullptr;
     return el;
 }
 
-void Polinom::appendItem(Polinom::PElement * head, Polinom::PElement * el) {
-    PElement * tmp = head;
-    while ( tmp->next != nullptr) tmp = tmp->next;
+void Polinom::appendItem(Polinom::PElement *head, Polinom::PElement *el) {
+    PElement *tmp = head;
+    while (tmp->next != nullptr) tmp = tmp->next;
     tmp->next = el;
 }
 
 void Polinom::printPol() {
-    PElement * tmp = this->head;
+    PElement *tmp = this->head;
     int i = 0, firstElIs0;
-    while ( tmp != nullptr) {
+    while (tmp != nullptr) {
         //firstElIs0 = tmp->key;
         if (tmp->key == 0) {
             tmp = tmp->next;
             i++;
             continue;
         }
-        if ((tmp->key > 0)&&(i != 0)) cout << "+";
+        if ((tmp->key > 0) && (i != 0)) cout << "+";
         cout << tmp->key;
         if (i != 0) cout << "x^" << i;
         tmp = tmp->next;
@@ -55,12 +55,12 @@ void Polinom::printPol() {
 }
 
 Polinom::~Polinom() {
-    if (this->head->next)
-        delete this->head->next;
+    if (head)
+        delete head->next;
     cout << "in destructor" << endl;
 }
 
-Polinom::PElement * Polinom::getHead() const {
+Polinom::PElement *Polinom::getHead() const {
     return head;
 }
 
@@ -76,16 +76,16 @@ void Polinom::setPower(int _power) {
     power = _power;
 }
 
-Polinom Polinom::addingPolinoms(Polinom& pol1, Polinom& pol2) {
+Polinom Polinom::addingPolinoms(Polinom &pol1, Polinom &pol2) {
     Polinom pol3;
 
-    power = (pol1.power>pol2.power)?pol1.power:pol2.power;
+    power = (pol1.power > pol2.power) ? pol1.power : pol2.power;
     head = makeItem(pol1.head->key + pol2.head->key);
 
-    PElement * tmp1 = pol1.head->next;
-    PElement * tmp2 = pol2.head->next;
+    PElement *tmp1 = pol1.head->next;
+    PElement *tmp2 = pol2.head->next;
 
-    while((tmp1)&&(tmp2)) {
+    while ((tmp1) && (tmp2)) {
         appendItem(head, makeItem(tmp1->key + tmp2->key));
         tmp1 = tmp1->next;
         tmp2 = tmp2->next;
@@ -94,23 +94,23 @@ Polinom Polinom::addingPolinoms(Polinom& pol1, Polinom& pol2) {
         appendItem(head, makeItem(tmp1->key));
         tmp1 = tmp1->next;
     }
-    while(tmp2) {
+    while (tmp2) {
         appendItem(head, makeItem(tmp2->key));
         tmp2 = tmp2->next;
     }
     return pol3;
 }
 
-Polinom Polinom::differencePolinom(Polinom& pol1, Polinom& pol2) {
+Polinom Polinom::differencePolinom(Polinom &pol1, Polinom &pol2) {
     Polinom pol3;
 
-    power = (pol1.power>pol2.power)?pol1.power:pol2.power;
+    power = (pol1.power > pol2.power) ? pol1.power : pol2.power;
     head = makeItem(pol1.head->key - pol2.head->key);
 
-    PElement * tmp1 = pol1.head->next;
-    PElement * tmp2 = pol2.head->next;
+    PElement *tmp1 = pol1.head->next;
+    PElement *tmp2 = pol2.head->next;
 
-    while((tmp1)&&(tmp2)) {
+    while ((tmp1) && (tmp2)) {
         appendItem(head, makeItem(tmp1->key - tmp2->key));
         tmp1 = tmp1->next;
         tmp2 = tmp2->next;
@@ -119,7 +119,7 @@ Polinom Polinom::differencePolinom(Polinom& pol1, Polinom& pol2) {
         appendItem(head, makeItem(tmp1->key));
         tmp1 = tmp1->next;
     }
-    while(tmp2) {
+    while (tmp2) {
         appendItem(head, makeItem(tmp2->key * (-1)));
         tmp2 = tmp2->next;
     }
@@ -131,14 +131,14 @@ Polinom Polinom::multiplicatePolinom(Polinom &pol1, Polinom &pol2) {
     int pow = pol1.power * pol2.power - 1;
     std::vector<int> num(pow + 1);
 
-    PElement * tmp1 = pol1.head;
-    PElement * tmp2 = pol2.head;
+    PElement *tmp1 = pol1.head;
+    PElement *tmp2 = pol2.head;
     int i = 0, j = 0;
 
     while (tmp1) {
         while (tmp2) {
-            if ((i+j) < (power + 1))
-                num[i+j] += tmp1->key * tmp2->key;
+            if ((i + j) < (power + 1))
+                num[i + j] += tmp1->key * tmp2->key;
             j++;
             tmp2 = tmp2->next;
         }
