54ea034e879c8698afaa05feb75e3b1a4b135e83
darinamazur <allottcentr@gmail.com>
2020-05-29 21:36:55 +0300

    add files

---
+++ Field.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Ring.h"
+
+
+class Field :public Ring {
+private:
+	
+	//Q is reducible
+	//p is prime
+public:
+	Field(int p);
+	// /
+	// %
+	// gcd
+	// inverse
+};
---
+++ Ring.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Polynom.h"
+
+
+class Ring : public Polynom {
+private:
+
+protected:
+	// a vector of irreducible polynomials
+	std::vector<Polynom> polynomials;
+	// R(p^n)[x]/Q
+	Polynom Q;//could be irreducible
+	//p could be prime or not
+public:
+	//+
+	//-
+	//*
+	//==
+	//!=
+};
